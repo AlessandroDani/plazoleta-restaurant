16225fd0d7f74770a29fcd8cbe474162fd11b292
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-01 22:42:19 -0500

    refactor(api): change saveRestaurant return type to void

--- src/main/java/com/pragma/powerup/domain/spi/IRestaurantPersistencePort.java
+++ src/main/java/com/pragma/powerup/domain/spi/IRestaurantPersistencePort.java
@@ -3,5 +3,5 @@ package com.pragma.powerup.domain.spi;
 import com.pragma.powerup.domain.model.Restaurant;
 
 public interface IRestaurantPersistencePort {
-    Restaurant saveRestaurant(Restaurant restaurant);
+    void saveRestaurant(Restaurant restaurant);
 }
--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/RestaurantJpaAdapter.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/RestaurantJpaAdapter.java
@@ -2,7 +2,6 @@ package com.pragma.powerup.infrastructure.out.jpa.adapter;
 
 import com.pragma.powerup.domain.model.Restaurant;
 import com.pragma.powerup.domain.spi.IRestaurantPersistencePort;
-import com.pragma.powerup.infrastructure.out.jpa.entity.RestaurantEntity;
 import com.pragma.powerup.infrastructure.out.jpa.mapper.IRestaurantEntityMapper;
 import com.pragma.powerup.infrastructure.out.jpa.repository.IRestaurantRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +13,7 @@ public class RestaurantJpaAdapter implements IRestaurantPersistencePort {
     private final IRestaurantEntityMapper restaurantEntityMapper;
 
     @Override
-    public Restaurant saveRestaurant(Restaurant restaurant) {
-        RestaurantEntity restaurantEntity = restaurantRepository.save(restaurantEntityMapper.toEntity(restaurant));
-        return restaurantEntityMapper.toRestaurant(restaurantEntity);
+    public void saveRestaurant(Restaurant restaurant) {
+        restaurantRepository.save(restaurantEntityMapper.toEntity(restaurant));
     }
 }
