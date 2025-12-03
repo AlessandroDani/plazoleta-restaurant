e4f010927dbfb85822b12f0ba9d39ecee8dd04b5
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-02 19:17:28 -0500

    feat(test): complete unit test

--- src/test/java/com/pragma/powerup/domain/usecase/RestaurantUseCaseTest.java
+++ src/test/java/com/pragma/powerup/domain/usecase/RestaurantUseCaseTest.java
@@ -3,11 +3,17 @@ package com.pragma.powerup.domain.usecase;
 import com.pragma.powerup.domain.model.Restaurant;
 import com.pragma.powerup.domain.spi.IRestaurantPersistencePort;
 import com.pragma.powerup.domain.spi.IUserGatewayPort;
+import com.pragma.powerup.infrastructure.exception.UserServiceCommunicationException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class RestaurantUseCaseTest {
@@ -22,7 +28,6 @@ class RestaurantUseCaseTest {
     private RestaurantUseCase restaurantUseCase;
 
     private Restaurant testRestaurant;
-    private Restaurant existingRestaurant;
 
     @BeforeEach
     void setUp() {
@@ -33,13 +38,28 @@ class RestaurantUseCaseTest {
         testRestaurant.setPhoneNumber("+573001234567");
         testRestaurant.setUrlLogo("http://logo.com/rc.png");
         testRestaurant.setIdOwner(2L);
+    }
 
-        existingRestaurant = new Restaurant();
-        existingRestaurant.setName("Restaurante Existente");
-        existingRestaurant.setNit("987654321");
-        existingRestaurant.setAddress("Av. Principal");
-        existingRestaurant.setPhoneNumber("+573009876543");
-        existingRestaurant.setUrlLogo("http://logo.com/re.png");
-        existingRestaurant.setIdOwner(2L);
+    @Test
+    void saveRestaurant_Success() {
+        when(userGatewayPort.isUserOwner(anyLong())).thenReturn(true);
+
+        when(restaurantPersistencePort.getRestaurantByNit(anyString())).thenReturn(null);
+
+        assertDoesNotThrow(() -> restaurantUseCase.saveRestaurant(testRestaurant));
+
+        verify(restaurantPersistencePort, times(1)).saveRestaurant(testRestaurant);
+    }
+
+    @Test
+    void saveRestaurant_UserServiceError_ThrowsException() {
+        when(userGatewayPort.isUserOwner(anyLong()))
+                .thenThrow(new UserServiceCommunicationException());
+
+        assertThrows(UserServiceCommunicationException.class, () ->
+                restaurantUseCase.saveRestaurant(testRestaurant)
+        );
+
+        verify(restaurantPersistencePort, never()).saveRestaurant(any(Restaurant.class));
     }
 }
