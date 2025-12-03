0c60c74ca01c27d273b6c6d86157b6268bbcc5c2
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-02 21:17:33 -0500

    feat(plate): complete domain layer

---
+++ src/main/java/com/pragma/powerup/domain/usecase/PlateUseCase.java
@@ -0,0 +1,20 @@
+package com.pragma.powerup.domain.usecase;
+
+import com.pragma.powerup.domain.api.IPlateServicePort;
+import com.pragma.powerup.domain.model.Plate;
+import com.pragma.powerup.domain.spi.IPlatePersistencePort;
+
+
+public class PlateUseCase implements IPlateServicePort {
+
+    private final IPlatePersistencePort platePersistencePort;
+
+    public PlateUseCase(IPlatePersistencePort platePersistencePort) {
+        this.platePersistencePort = platePersistencePort;
+    }
+
+    @Override
+    public void savePlate(Plate plate) {
+        platePersistencePort.savePlate(plate);
+    }
+}
