612fe459a20d9c9fc666f387c5da851bc4faebcf
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-03 22:04:31 -0500

    feat(test): add unit test

--- src/main/java/com/pragma/powerup/application/dto/request/PlateUpdateRequestDto.java
+++ src/main/java/com/pragma/powerup/application/dto/request/PlateUpdateRequestDto.java
@@ -11,13 +11,11 @@ import javax.validation.constraints.Min;
 @Setter
 public class PlateUpdateRequestDto {
     @Schema(description = "Precio del plato, debe ser mayor a cero", example = "15000")
-//    @NotNull(message = "El precio es obligatorio")
     @Min(value = 1, message = "El precio debe ser numero entero positivo mayor a 0")
     @JsonProperty("precio")
     private Long price;
 
     @Schema(description = "Descripción detallada del plato", example = "Carne de res, queso, lechuga y salsa.")
-//    @NotBlank(message = "La descripción es obligatoria")
     @JsonProperty("descripcion")
     private String description;
 }
