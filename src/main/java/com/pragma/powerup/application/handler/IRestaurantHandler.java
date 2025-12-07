7f39f5b4d60dab0a64559dc4ba421832d27d5115
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-06 23:30:25 -0500

    feat(restaurant): complete paged list implementation in Application and Infrastructure

--- src/main/java/com/pragma/powerup/application/handler/IRestaurantHandler.java
+++ src/main/java/com/pragma/powerup/application/handler/IRestaurantHandler.java
@@ -1,8 +1,13 @@
 package com.pragma.powerup.application.handler;
 
 import com.pragma.powerup.application.dto.request.RestaurantRequestDto;
+import com.pragma.powerup.application.dto.response.RestaurantResponseClientDto;
+
+import java.util.List;
 
 public interface IRestaurantHandler {
 
     void save(RestaurantRequestDto restaurantRequestDto);
+    List<RestaurantResponseClientDto> getAllRestaurant(int page, int size);
+
 }
--- src/main/java/com/pragma/powerup/application/handler/impl/RestaurantHandler.java
+++ src/main/java/com/pragma/powerup/application/handler/impl/RestaurantHandler.java
@@ -1,13 +1,17 @@
 package com.pragma.powerup.application.handler.impl;
 
 import com.pragma.powerup.application.dto.request.RestaurantRequestDto;
+import com.pragma.powerup.application.dto.response.RestaurantResponseClientDto;
 import com.pragma.powerup.application.handler.IRestaurantHandler;
 import com.pragma.powerup.application.mapper.IRestaurantRequestMapper;
+import com.pragma.powerup.application.mapper.IRestaurantResponseMapper;
 import com.pragma.powerup.domain.api.IRestaurantServicePort;
 import com.pragma.powerup.domain.model.Restaurant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,11 +19,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class RestaurantHandler implements IRestaurantHandler{
 
     private final IRestaurantRequestMapper restaurantRequestMapper;
+    private final IRestaurantResponseMapper restaurantResponseMapper;
     private final IRestaurantServicePort restaurantServicePort;
 
     @Override
     public void save(RestaurantRequestDto restaurantRequestDto) {
         Restaurant restaurant = restaurantRequestMapper.toRestaurant(restaurantRequestDto);
         restaurantServicePort.saveRestaurant(restaurant);
+    }
+
+    @Override
+    public List<RestaurantResponseClientDto> getAllRestaurant(int page, int size) {
+        return restaurantResponseMapper.toResponseList(restaurantServicePort.getAllRestaurant(page, size));
     }
 }
