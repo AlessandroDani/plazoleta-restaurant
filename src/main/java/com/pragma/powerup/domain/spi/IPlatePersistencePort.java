eec24dda9f635271caccd31dfa692f2b74fb7a78
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-07 15:07:58 -0500

    feat(plate): implement paged and filtered plate listing in Domain layer

--- src/main/java/com/pragma/powerup/domain/spi/IPlatePersistencePort.java
+++ src/main/java/com/pragma/powerup/domain/spi/IPlatePersistencePort.java
@@ -2,9 +2,12 @@ package com.pragma.powerup.domain.spi;
 
 import com.pragma.powerup.domain.model.Plate;
 
+import java.util.List;
+
 public interface IPlatePersistencePort {
     void savePlate(Plate plate);
     Plate getPlateById(Long id);
     Plate getPlateByName(String name);
     void updatePlate(Plate plate);
+    List<Plate> getPlatesByRestaurant(Long idRestaurant, int page, int size, String category);
 }
