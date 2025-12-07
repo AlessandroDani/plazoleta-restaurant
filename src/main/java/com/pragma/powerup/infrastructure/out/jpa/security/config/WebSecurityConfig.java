2fe6b34d123d171d089449f4ed5bec89d8ddaf25
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-06 22:56:10 -0500

    refactor(security): apply RESTful conventions and role-based protection to /platos /restaurantes endpoints

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/security/config/WebSecurityConfig.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/security/config/WebSecurityConfig.java
@@ -20,8 +20,8 @@ public class WebSecurityConfig {
     private final JwtFilter jwtFilter;
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
-    private static final String ADMINISTRADOR = "ADMINISTRADOR";
-    private static final String PROPIETARIO = "PROPIETARIO";
+    private static final String ADMIN = "ADMINISTRADOR";
+    private static final String OWNER = "PROPIETARIO";
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -33,10 +33,11 @@ public class WebSecurityConfig {
                 .accessDeniedHandler(customAccessDeniedHandler)
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/api/restaurante").hasRole(ADMINISTRADOR)
-                .antMatchers(HttpMethod.POST, "/api/plato").hasRole(PROPIETARIO)
-                .antMatchers(HttpMethod.PUT, "/api/plato/**").hasRole(PROPIETARIO)
-                .antMatchers(HttpMethod.PATCH, "/api/plato/habilitar/**").hasRole(PROPIETARIO)
+                .antMatchers(HttpMethod.POST, "/api/restaurantes").hasRole(ADMIN)
+                .antMatchers(HttpMethod.GET, "/api/restaurantes").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/platos").hasRole(OWNER)
+                .antMatchers(HttpMethod.PUT, "/api/platos/{id}").hasRole(OWNER)
+                .antMatchers(HttpMethod.PATCH, "/api/platos/{id}").hasRole(OWNER)
                 .antMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .anyRequest()
                 .authenticated()
