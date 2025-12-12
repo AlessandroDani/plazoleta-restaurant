8e8be8764d79010c3ce3aff67cdfa42e4918dd34
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-12 11:53:19 -0500

    feat(order): implement persistence and service port for getOrdersByRestaurantAndStatus in use case

--- src/main/java/com/pragma/powerup/domain/spi/IOrderPersistencePort.java
+++ src/main/java/com/pragma/powerup/domain/spi/IOrderPersistencePort.java
@@ -2,8 +2,13 @@ package com.pragma.powerup.domain.spi;
 
 
 import com.pragma.powerup.domain.model.Order;
+import com.pragma.powerup.domain.model.OrderStatus;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface IOrderPersistencePort {
     void saveOrder(Order order);
     boolean hasActiveOrder(Long userId);
+    Optional<List<Order>> getOrdersByRestaurantAndStatus(Long restaurantId, OrderStatus status, int page, int size);
 }
--- src/main/java/com/pragma/powerup/domain/usecase/OrderUseCase.java
+++ src/main/java/com/pragma/powerup/domain/usecase/OrderUseCase.java
@@ -1,29 +1,30 @@
 package com.pragma.powerup.domain.usecase;
 
 import com.pragma.powerup.domain.api.IOrderServicePort;
+import com.pragma.powerup.domain.exception.EmployeeNotValidException;
+import com.pragma.powerup.domain.exception.OrderNotFoundException;
 import com.pragma.powerup.domain.exception.RestaurantNotExistException;
 import com.pragma.powerup.domain.exception.UserHasActiveOrderException;
 import com.pragma.powerup.domain.model.*;
-import com.pragma.powerup.domain.spi.IOrderPersistencePort;
-import com.pragma.powerup.domain.spi.IPlatePersistencePort;
-import com.pragma.powerup.domain.spi.IRestaurantPersistencePort;
-import com.pragma.powerup.domain.spi.ITokenPort;
+import com.pragma.powerup.domain.spi.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class OrderUseCase implements IOrderServicePort {
     private final IOrderPersistencePort orderPersistencePort;
     private final IRestaurantPersistencePort restaurantPersistencePort;
     private final ITokenPort tokenPort;
     private final IPlatePersistencePort platePersistencePort;
+    private final IRestaurantEmployeePersistencePort restaurantEmployeePersistencePort;
 
-    public OrderUseCase(IOrderPersistencePort orderPersistencePort, IRestaurantPersistencePort restaurantPersistencePort, ITokenPort tokenPort, IPlatePersistencePort platePersistencePort) {
+    public OrderUseCase(IOrderPersistencePort orderPersistencePort, IRestaurantPersistencePort restaurantPersistencePort, ITokenPort tokenPort, IPlatePersistencePort platePersistencePort, IRestaurantEmployeePersistencePort restaurantEmployeePersistencePort) {
         this.orderPersistencePort = orderPersistencePort;
         this.restaurantPersistencePort = restaurantPersistencePort;
         this.tokenPort = tokenPort;
         this.platePersistencePort = platePersistencePort;
+        this.restaurantEmployeePersistencePort = restaurantEmployeePersistencePort;
     }
-
 
     @Override
     public void saveOrder(Order order) {
@@ -34,6 +35,15 @@ public class OrderUseCase implements IOrderServicePort {
 
         order.initializeNewOrder(userId, LocalDate.now());
         orderPersistencePort.saveOrder(order);
+    }
+
+    @Override
+    public List<Order> getOrdersByStatus(OrderStatus status, int page, int  size) {
+        Long userId = tokenPort.getUserId();
+        RestaurantEmployee employee = restaurantEmployeePersistencePort.getEmployee(userId)
+                .orElseThrow(EmployeeNotValidException::new);
+        return orderPersistencePort.getOrdersByRestaurantAndStatus(employee.getIdRestaurant(), status, page, size)
+                .orElseThrow(OrderNotFoundException::new);
     }
 
     private Restaurant validateRestaurant(Long idRestaurant) {
