6dc8920e153be773eeddd3545b6ed9549b831e80
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-11 20:27:15 -0500

    feat(employee): implement RestaurantEmployee creation logic in Domain

---
+++ src/main/java/com/pragma/powerup/domain/usecase/RestaurantEmployeeUseCase.java
@@ -0,0 +1,35 @@
+package com.pragma.powerup.domain.usecase;
+
+import com.pragma.powerup.domain.api.IRestaurantEmployeeServicePort;
+import com.pragma.powerup.domain.exception.RestaurantNotExistException;
+import com.pragma.powerup.domain.model.Restaurant;
+import com.pragma.powerup.domain.model.RestaurantEmployee;
+import com.pragma.powerup.domain.spi.IRestaurantEmployeePersistencePort;
+import com.pragma.powerup.domain.spi.IRestaurantPersistencePort;
+import com.pragma.powerup.domain.spi.ITokenPort;
+import com.pragma.powerup.domain.spi.IUserGatewayPort;
+
+public class RestaurantEmployeeUseCase implements IRestaurantEmployeeServicePort {
+
+    private final IRestaurantEmployeePersistencePort restaurantEmployeePersistencePort;
+    private final IRestaurantPersistencePort restaurantPersistencePort;
+    private final ITokenPort tokenPort;
+    private final IUserGatewayPort  userGatewayPort;
+
+    public RestaurantEmployeeUseCase(IRestaurantEmployeePersistencePort restaurantEmployeePersistencePort, IRestaurantPersistencePort restaurantPersistencePort, ITokenPort tokenPort, IUserGatewayPort userGatewayPort) {
+        this.restaurantEmployeePersistencePort = restaurantEmployeePersistencePort;
+        this.restaurantPersistencePort = restaurantPersistencePort;
+        this.tokenPort = tokenPort;
+        this.userGatewayPort = userGatewayPort;
+    }
+
+    @Override
+    public void saveEmployee(RestaurantEmployee restaurantEmployee) {
+        Restaurant restaurant = restaurantPersistencePort.getRestaurantById(restaurantEmployee.getIdRestaurant())
+                .orElseThrow(RestaurantNotExistException::new);
+
+        restaurant.validateOwner(tokenPort.getUserId());
+        userGatewayPort.isUserEmployee(restaurantEmployee.getIdUser());
+        restaurantEmployeePersistencePort.saveEmployee(restaurantEmployee);
+    }
+}
