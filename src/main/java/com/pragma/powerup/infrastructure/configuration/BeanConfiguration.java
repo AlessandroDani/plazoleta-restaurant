3542f2af41c26fb96f7bd9fd651ede11a0c32920
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-12 11:54:18 -0500

    feat(config): add RestaurantEmployeePersistencePort bean to BeanConfiguration

--- src/main/java/com/pragma/powerup/infrastructure/configuration/BeanConfiguration.java
+++ src/main/java/com/pragma/powerup/infrastructure/configuration/BeanConfiguration.java
@@ -59,8 +59,8 @@ public class BeanConfiguration {
     }
 
     @Bean
-    public IOrderServicePort orderServicePort(IOrderPersistencePort orderPersistencePort, IRestaurantPersistencePort restaurantPersistencePort, ITokenPort tokenPort, IPlatePersistencePort platePersistencePort) {
-        return new OrderUseCase(orderPersistencePort, restaurantPersistencePort, tokenPort, platePersistencePort);
+    public IOrderServicePort orderServicePort(IOrderPersistencePort orderPersistencePort, IRestaurantPersistencePort restaurantPersistencePort, ITokenPort tokenPort, IPlatePersistencePort platePersistencePort, IRestaurantEmployeePersistencePort restaurantEmployeePersistencePort) {
+        return new OrderUseCase(orderPersistencePort, restaurantPersistencePort, tokenPort, platePersistencePort, restaurantEmployeePersistencePort);
 
     }
 
