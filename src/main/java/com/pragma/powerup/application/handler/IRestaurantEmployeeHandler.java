72700d660a69fb4eada64ba1a660366d5bde25eb
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-11 20:29:58 -0500

    feat(handler): implement RestaurantEmployee Handler and Mappers

---
+++ src/main/java/com/pragma/powerup/application/handler/IRestaurantEmployeeHandler.java
@@ -0,0 +1,8 @@
+package com.pragma.powerup.application.handler;
+
+
+import com.pragma.powerup.application.dto.request.RestaurantEmployeeRequestDto;
+
+public interface IRestaurantEmployeeHandler {
+    void saveEmployee(RestaurantEmployeeRequestDto restaurantEmployeeRequestDto);
+}
---
+++ src/main/java/com/pragma/powerup/application/handler/impl/RestaurantEmployeeHandler.java
@@ -0,0 +1,24 @@
+package com.pragma.powerup.application.handler.impl;
+
+import com.pragma.powerup.application.dto.request.RestaurantEmployeeRequestDto;
+import com.pragma.powerup.application.handler.IRestaurantEmployeeHandler;
+import com.pragma.powerup.application.mapper.IRestaurantEmployeeRequestMapper;
+import com.pragma.powerup.domain.api.IRestaurantEmployeeServicePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class RestaurantEmployeeHandler implements IRestaurantEmployeeHandler {
+
+    private final IRestaurantEmployeeRequestMapper restaurantEmployeeRequestMapper;
+    private final IRestaurantEmployeeServicePort restaurantEmployeeServicePort;
+
+    @Override
+    public void saveEmployee(RestaurantEmployeeRequestDto restaurantEmployeeRequestDto) {
+        restaurantEmployeeServicePort.saveEmployee(restaurantEmployeeRequestMapper.toRestaurantEmployee(restaurantEmployeeRequestDto));
+    }
+}
