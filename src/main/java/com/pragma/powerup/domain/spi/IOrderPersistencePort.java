d7d4abeaed814e18fde15ce95de8751a5ea500e1
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-13 12:07:18 -0500

    feat(order): implement getOrderById functionality

--- src/main/java/com/pragma/powerup/domain/spi/IOrderPersistencePort.java
+++ src/main/java/com/pragma/powerup/domain/spi/IOrderPersistencePort.java
@@ -5,9 +5,11 @@ import com.pragma.powerup.domain.model.Order;
 import com.pragma.powerup.domain.model.OrderStatus;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IOrderPersistencePort {
     void saveOrder(Order order);
     boolean hasActiveOrder(Long userId);
+    Optional<Order> getOrderById(Long orderId);
     List<Order> getOrdersByRestaurantAndStatus(Long restaurantId, OrderStatus status, int page, int size);
 }
