19cd8c591375d2172fbf5046b52fd3c3369c29ee
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-13 21:48:33 -0500

    feat(infrastructure): add SMS request DTO and Feign client for user-service

--- src/main/java/com/pragma/powerup/infrastructure/out/http/feign/IUserFeignClient.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/http/feign/IUserFeignClient.java
@@ -1,12 +1,19 @@
 package com.pragma.powerup.infrastructure.out.http.feign;
 
+import com.pragma.powerup.infrastructure.out.http.request.SmsRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "user-service", url = "http://localhost:8082")
 public interface IUserFeignClient {
 
     @GetMapping("/api/usuarios/check-role/{userId}/{roleName}")
     void checkRole(@PathVariable("userId") Long userId, @PathVariable("roleName") String roleName);
+
+    @PostMapping("/api/v1/sms/send")
+    void sendSms(@RequestBody SmsRequestDto request);
+
 }
