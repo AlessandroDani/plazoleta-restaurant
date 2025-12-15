0555c6ef2163371f024c1972582aad00e10530eb
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-15 11:35:57 -0500

    feat(traceability-feign): add GET endpoint to retrieve order traceability by ID

--- src/main/java/com/pragma/powerup/infrastructure/out/http/feign/ITraceabilityFeignClient.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/http/feign/ITraceabilityFeignClient.java
@@ -3,8 +3,10 @@ package com.pragma.powerup.infrastructure.out.http.feign;
 import com.pragma.powerup.infrastructure.out.http.request.TraceabilityRequestDto;
 import com.pragma.powerup.infrastructure.out.http.response.EmployeePerformanceResponseDto;
 import com.pragma.powerup.infrastructure.out.http.response.OrderEfficiencyResponseDto;
+import com.pragma.powerup.infrastructure.out.http.response.TraceabilityResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,6 +15,9 @@ public interface ITraceabilityFeignClient {
 
     @PostMapping("api/trazabilidad")
     void saveOrderTrace(@RequestBody TraceabilityRequestDto trace);
+
+    @GetMapping("api/trazabilidad/{id}")
+    TraceabilityResponseDto getOrderTrace(@PathVariable Long id);
 
     @GetMapping("api/trazabilidad/eficiencia/pedidos")
     OrderEfficiencyResponseDto getOrdersEfficiency();
