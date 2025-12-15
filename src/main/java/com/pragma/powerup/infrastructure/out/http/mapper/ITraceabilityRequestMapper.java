a974468c98a1c523aa013c5849c06ad4e71e7965
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-15 00:03:05 -0500

    feat(traceability-model): create Traceability Domain Model, Request DTO, and Infrastructure Mapper

---
+++ src/main/java/com/pragma/powerup/infrastructure/out/http/mapper/ITraceabilityRequestMapper.java
@@ -0,0 +1,22 @@
+package com.pragma.powerup.infrastructure.out.http.mapper;
+
+import com.pragma.powerup.domain.model.EmployeePerformance;
+import com.pragma.powerup.domain.model.OrderEfficiency;
+
+import com.pragma.powerup.domain.model.Traceability;
+import com.pragma.powerup.infrastructure.out.http.request.TraceabilityRequestDto;
+import com.pragma.powerup.infrastructure.out.http.response.EmployeePerformanceResponseDto;
+import com.pragma.powerup.infrastructure.out.http.response.OrderEfficiencyResponseDto;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface ITraceabilityRequestMapper {
+
+    TraceabilityRequestDto toRequestDto(Traceability traceability);
+    EmployeePerformance toModel(EmployeePerformanceResponseDto employeePerformanceResponseDto);
+    OrderEfficiency toModel(OrderEfficiencyResponseDto orderEfficiencyResponseDto);
+}
