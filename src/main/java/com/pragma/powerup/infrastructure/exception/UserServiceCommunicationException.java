0300cab6e54a2b9168b63ed303f6d32234073c09
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-16 22:29:56 -0500

    refactor(adapter): centralize external communication in executeExternalCall

--- src/main/java/com/pragma/powerup/infrastructure/exception/UserServiceCommunicationException.java
+++
@@ -1,7 +0,0 @@
-package com.pragma.powerup.infrastructure.exception;
-
-public class UserServiceCommunicationException extends RuntimeException {
-    public UserServiceCommunicationException() {
-        super();
-    }
-}
