d6cfeac04df96ad795b6652d48be5c2909f246af
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-12-20 16:24:27 -0500

    refactor: remove manual stream implementation for Plate list mapping

--- src/main/java/com/pragma/powerup/application/mapper/IPlateResponseMapper.java
+++ src/main/java/com/pragma/powerup/application/mapper/IPlateResponseMapper.java
@@ -6,17 +6,11 @@ import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface IPlateResponseMapper {
     PlateResponseDto toResponse(Plate plate);
-
-    default List<PlateResponseDto> toResponseList(List<Plate> plates) {
-        return plates.stream()
-                .map(this::toResponse)
-                .collect(Collectors.toList());
-    }
+    List<PlateResponseDto> toResponseList(List<Plate> plates);
 }
